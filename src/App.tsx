87af90b6fb38ab770304e11a52c1ab8a4343d769
Дмитрий Никифоров <dnikiforov@innoseti.ru>
2021-10-23 22:07:57 +0200

    feat: added button timer, added button status change

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  useAdaptivity,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  ViewWidth,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  Div,
+  Button,
+  Counter,
+} from "@vkontakte/vkui";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const { viewWidth } = useAdaptivity();
+  const [currentFlashId, setCurrentFlashId] = useState(1);
+  const [flashLights] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (currentFlashId === 8) {
+        setCurrentFlashId(1);
+      } else {
+        setCurrentFlashId(currentFlashId + 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+
+  const changeActive = (id: number): any => {
+    flashLights[id] = !flashLights[id];
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppRoot mode="full">
+      <SplitLayout header={<PanelHeader separator={false} />}>
+        <SplitCol spaced={viewWidth && viewWidth > ViewWidth.MOBILE}>
+          <View activePanel="main">
+            <Panel id="main">
+              <header>
+                <PanelHeader>Абчихба</PanelHeader>
+              </header>
+              <main>
+                <Group
+                  header={
+                    <Header mode="secondary">Управление фонариком</Header>
+                  }
+                >
+                  {flashLights.map(
+                    (flashLightState: boolean, index: number) => (
+                      <Div
+                        style={{ display: "flex" }}
+                        key={`flashlightbutton_${index + 1}`}
+                      >
+                        <Button
+                          id={(index + 1).toString()}
+                          mode="outline"
+                          className={`${
+                            currentFlashId === index + 1 ? "light" : ""
+                          } ${flashLightState ? "active" : "not-active"}`}
+                          stretched
+                          size="l"
+                          after={<Counter>{index + 1}</Counter>}
+                          onClick={() => changeActive(index)}
+                        />
+                      </Div>
+                    )
+                  )}
+                </Group>
+              </main>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
   );
 }
 
